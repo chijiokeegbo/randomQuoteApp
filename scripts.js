7bd2b1417d00e3e9edfb560ab32690ab6b54409a
Chijioke <chijioke.egbo@gmail.com>
2022-09-16 21:00:09 +0100

    first commitment

---
+++ scripts.js
@@ -0,0 +1,72 @@
+
+function App(){
+    const [quotes, setQuotes] = React.useState([]);
+    const [randomQuote, setRandomQuote] = React.useState([""]);
+
+    React.useEffect(() => {
+        async function fetchData(){
+            //fetch the quotes from the website
+            const respones = await fetch("https://type.fit/api/quotes");
+            const data = await respones.json();
+
+            setQuotes(data);
+            let randIndex = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randIndex]);
+        }
+
+        fetchData();
+
+    }, [])
+
+    const getNewQuote = () => {
+        let randIndex = Math.floor(Math.random() * quotes.length);
+        setRandomQuote(quotes[randIndex]);
+    }
+
+    return (
+        <div className="container pt-5">
+            <div className="jumbotron">
+                <div className="card">
+                    <div className="card-header">
+                        Inspirational Quotes
+                    </div>
+                    <div className="card-body">
+                        {
+                            randomQuote ? (
+                                <>
+                                <p className="card-tex">&quot;{randomQuote.text}&quot;</p>
+                                <h5 className="card-title">-{randomQuote.author || "No Author"}</h5>
+                                </>
+                                
+                               
+                             ) : (
+                                 <h2>Loading</h2>
+                              )
+                        }
+
+                        <div className="row">
+                            <a className="btn btn-warning"
+                            href={""}endcodeURIComponent {
+                                '"' + randomQuote.text + '"' + randomQuote.author}
+                            target="_blank">
+                                <i className="fas fa-twitter"></i>
+                            </a>
+                            <a className="btn btn-warning ml-1">
+                                <i className="fa fa-tumblr"></i>
+                            </a>
+                            <button onClick={getNewQuote} className="btn btn-primary ml-3">New Quote</button>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+            
+        </div>
+        
+
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
